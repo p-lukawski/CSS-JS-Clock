559f6551cda20d28e871d6e81d27b0387546dc44
p-lukawski <31187185+p-lukawski@users.noreply.github.com>
2017-12-14 17:46:53 +0100

    clock face and hands update

--- js/app.js
+++ js/app.js
@@ -6,16 +6,16 @@ document.addEventListener('DOMContentLoaded', function () {
        const now = new Date();
        const sec = now.getSeconds();
        const secDeg = ((sec / 60) * 360) + 90;
-       secondHand.style.transform = `rotate(${secDeg}deg)`;
+       secondHand.style.transform = 'rotate(' + secDeg + 'deg)';
 
        const min = now.getMinutes();
        const minDeg = ((min / 60) * 360) + ((sec / 60) * 6) + 90;
-       minsHand.style.transform = `rotate(${minDeg}deg)`;
+       minsHand.style.transform = 'rotate(' + minDeg + 'deg)';
 
        const hour = now.getHours();
        const hourDeg = ((hour / 12) * 360) + ((min/60)*30) + 90;
-       hourHand.style.transform = `rotate(${hourDeg}deg)`;
+       hourHand.style.transform = 'rotate(' + hourDeg + 'deg';
    }
    setInterval(setDate, 1000);
-    
+
 });
